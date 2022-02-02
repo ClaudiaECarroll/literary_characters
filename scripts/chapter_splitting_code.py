dc68ae9b023e381d0ea89d5d984068cf664b6681
ClaudiaECarroll <95948642+ClaudiaECarroll@users.noreply.github.com>
2022-02-02 15:50:42 -0500

    add leading zeroes to file name

--- scripts/chapter_splitting_code.py
+++ scripts/chapter_splitting_code.py
@@ -9,6 +9,6 @@ text = str(text.read())
 chapters = re.split("Chapter ", text, flags = re.IGNORECASE)
 chapters.pop(0)
 for i in range(1, len(chapters)+1):
-	writetext = open(str(name[0]) + "_" + str(i) + ".txt", "w+")
+	writetext = open(str(name[0]) + "_" + str(i).zfill(4) + ".txt", "w+")
 	writetext.write(chapters[i-1])
 	writetext.close()
