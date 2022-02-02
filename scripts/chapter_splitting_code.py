bc42d2ef2b22bce61f2c6ed47c0cead33779d88d
ClaudiaECarroll <95948642+ClaudiaECarroll@users.noreply.github.com>
2022-02-02 15:27:12 -0500

    fix output filenames

--- scripts/chapter_splitting_code.py
+++ scripts/chapter_splitting_code.py
@@ -1,9 +1,14 @@
 import re
-text = open("filenameTh.txt", "r")
+import sys
+
+filename = sys.argv[1]
+name = filename.split(".")
+
+text = open(filename, "r")
 text = str(text.read())
-chapters = re.split("Chapter ", text, flags = re.IGNORECASE)
+chapters = re.split("CHAPTER ", text)
 chapters.pop(0)
 for i in range(1, len(chapters)+1):
-	writetext = open("{}.txt".format(i), "w+")
+	writetext = open(str(name[0]) + "_" + str(i) + ".txt", "w+")
 	writetext.write(chapters[i-1])
 	writetext.close()
