807021d91c6ff7ed564abb1e1dcd085366cd848a
ClaudiaECarroll <95948642+ClaudiaECarroll@users.noreply.github.com>
2022-02-02 15:40:25 -0500

    Update chapter_splitting_code.py

--- scripts/chapter_splitting_code.py
+++ scripts/chapter_splitting_code.py
@@ -6,7 +6,7 @@ name = filename.split(".")
 
 text = open(filename, "r")
 text = str(text.read())
-chapters = re.split("CHAPTER ", text)
+chapters = re.split("Chapter ", text, flags = re.IGNORECASE)
 chapters.pop(0)
 for i in range(1, len(chapters)+1):
 	writetext = open(str(name[0]) + "_" + str(i) + ".txt", "w+")
