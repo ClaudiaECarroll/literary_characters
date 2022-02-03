d5eb20cb1a47c6adfc42f07066317eeb959f900d
ClaudiaECarroll <95948642+ClaudiaECarroll@users.noreply.github.com>
2022-02-02 21:12:45 -0500

    trying to make regex work

--- scripts/chapter_splitting_code.py
+++ scripts/chapter_splitting_code.py
@@ -20,3 +20,11 @@ elif "CHAPTER" in text:
         writetext = open(str(name[0]) + "_" + str(i).zfill(4) + ".txt", "w+")
         writetext.write(chapters[i-1])
         writetext.close()
+#elif re.match("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"):
+    #pattern_split = re.compile(r"^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")
+    #chapters = pattern_split.split(text)
+    #chapters.pop(0)
+    #for i in range(1, len(chapters)+1):
+        #writetext = open(str(name[0]) + "_" + str(i).zfill(4) + ".txt", "w+")
+        #writetext.write(chapters[i-1])
+        #writetext.close()
