d3c48d76d9fa38df5e3a2dd1ed039d03a9cdc151
ClaudiaECarroll <95948642+ClaudiaECarroll@users.noreply.github.com>
2022-02-02 20:47:18 -0500

    Update chapter_splitting_code.py

--- scripts/chapter_splitting_code.py
+++ scripts/chapter_splitting_code.py
@@ -6,9 +6,17 @@ name = filename.split(".")
 
 text = open(filename, "r")
 text = str(text.read())
-chapters = re.split("Chapter ", text, flags = re.IGNORECASE)
-chapters.pop(0)
-for i in range(1, len(chapters)+1):
-	writetext = open(str(name[0]) + "_" + str(i).zfill(4) + ".txt", "w+")
-	writetext.write(chapters[i-1])
-	writetext.close()
+if "Chapter" in text:
+    chapters = re.split("Chapter ", text)
+    chapters.pop(0)
+    for i in range(1, len(chapters)+1):
+        writetext = open(str(name[0]) + "_" + str(i).zfill(4) + ".txt", "w+")
+        writetext.write(chapters[i-1])
+        writetext.close()
+elif "CHAPTER" in text:
+    chapters = re.split("CHAPTER ", text)
+    chapters.pop(0)
+    for i in range(1, len(chapters)+1):
+        writetext = open(str(name[0]) + "_" + str(i).zfill(4) + ".txt", "w+")
+        writetext.write(chapters[i-1])
+        writetext.close()
